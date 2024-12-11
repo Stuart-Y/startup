b597c60638b8874c29bd51f90cf6f187c94cb7fa
Stuart Yorgason <Stuart.Yorgason@gmail.com>
2024-12-11 13:45:56 -0700

    fixing circular reference in database connection

--- service/index.js
+++ service/index.js
@@ -1,10 +1,12 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
 const express = require('express');
-const uuid = require('uuid');
 const app = express();
+const DB = require('./database.js');
+
+const authCookieName = 'token';
 
 // The scores and users are saved in memory and disappear whenever the service is restarted.
-let users = {};
-let userItems = [];
 let fills = []
 
 // The service port. In production the front-end code is statically hosted by the service on the same port.
@@ -13,8 +15,12 @@ const port = process.argv.length > 2 ? process.argv[2] : 4000;
 // JSON body parsing using built-in middleware
 app.use(express.json());
 
+app.use(cookieParser());
+
 // Serve up the front-end static content hosting
 app.use(express.static('public'));
+
+app.set('trust proxy', true);
 
 // Router for service endpoints
 var apiRouter = express.Router();
@@ -22,26 +28,27 @@ app.use(`/api`, apiRouter);
 
 // CreateAuth a new user
 apiRouter.post('/auth/create', async (req, res) => {
-  
-  const user = users[req.body.email];
-  if (user) {
+  if (await DB.getUser(req.body.email)) {
     res.status(409).send({ msg: 'Existing user' });
   } else {
-    const user = { email: req.body.email, password: req.body.password, token: uuid.v4() };
-    console.log(user)
-    users[user.email] = user;
+    const user = await DB.createUser(req.body.email, req.body.password);
 
-    res.send({ token: user.token });
+    // Set the cookie
+    setAuthCookie(res, user.token);
+
+    res.send({
+      id: user._id,
+    });
   }
 });
 
 // GetAuth login an existing user
 apiRouter.post('/auth/login', async (req, res) => {
-  const user = users[req.body.email];
+  const user = await DB.getUser(req.body.email);
   if (user) {
-    if (req.body.password === user.password) {
-      user.token = uuid.v4();
-      res.send({ token: user.token });
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      setAuthCookie(res, user.token);
+      res.send({ id: user._id });
       return;
     }
   }
@@ -49,12 +56,22 @@ apiRouter.post('/auth/login', async (req, res) => {
 });
 
 // DeleteAuth logout a user
-apiRouter.delete('/auth/logout', (req, res) => {
-  const user = Object.values(users).find((u) => u.token === req.body.token);
-  if (user) {
-    delete user.token;
-  }
+apiRouter.delete('/auth/logout', (_req, res) => {
+  res.clearCookie(authCookieName);
   res.status(204).end();
+});
+
+const secureApiRouter = express.Router();
+apiRouter.use(secureApiRouter);
+
+secureApiRouter.use(async (req, res, next) => {
+  const authToken = req.cookies[authCookieName];
+  const user = await DB.getUserByToken(authToken);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
 });
 
 apiRouter.get('/fills', (_req, res) => {
@@ -76,7 +93,7 @@ function updateFills(newFill, fills) {
   return fills;
 }
 
-apiRouter.get('/customs', async (_req, res) => {
+secureApiRouter.get('/customs', async (_req, res) => {
   /*const user = users[req.query.user];
     const items = userItems[user];
     if (items) {
@@ -84,11 +101,11 @@ apiRouter.get('/customs', async (_req, res) => {
       return;
     }
     res.send({items: {}})*/
-  console.log(userItems)
+  const userItems = await DB.getItems();
   res.send(userItems);  
 });
 
-apiRouter.post('/custom', (req,res) => {
+secureApiRouter.post('/custom', async (req,res) => {
   /*const user = users[req.body.user];
       const item = req.body.item
       const items = userItems[user] || [];
@@ -101,23 +118,27 @@ apiRouter.post('/custom', (req,res) => {
       }
       userItems[user] = items
       console.log(userItems)*/
-      console.log(req.body)
-      console.log(userItems)
-      userItems.push(req.body)
-      console.log(userItems)
+      await DB.addItem(req.body)
+      const userItems = await DB.getItems(); 
       res.send(userItems)
   });
 
-function updateItems(newItem, userItems){
-  userItems.push(newItem)
-  console.log(userItems)
-  return userItems
-}  
+app.use(function (err, req, res, next) {
+  res.status(500).send({ type: err.name, message: err.message });
+});
 
 // Return the application's default page if the path is unknown
 app.use((_req, res) => {
   res.sendFile('index.html', { root: 'public' });
 });
+
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
